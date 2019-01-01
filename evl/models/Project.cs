03cb04b36acda3bcb671f550d0f56c618e173c0f
Dmitry <dmitri.b.98.08@mail.ru>
2019-01-01 20:09:12 +0700

    Всё подряд

---
+++ evl/models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evl.models
+{
+    class Project
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+
+        public Project(string name, string date, string description)
+        {
+            this.Name = name;
+            this.Date = date;
+            this.Description = description;
+        }
+    }
+}
