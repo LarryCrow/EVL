add0a7d9916856087e7e243d5522a46e61a7cbb3
Alexander Drozdov <drozdov.alexander.98@gmail.com>
2019-01-05 00:12:50 +0700

    restore cleaned

---
+++ Model/AnimalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Models
+{
+    public class Cat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class AnimalDbContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("Data Source=mydb.db;");
+    }
+}
