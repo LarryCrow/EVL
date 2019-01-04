343723552fb72efd2563b488858ce9a4bd6968b3
Dmitry <dmitri.b.98.08@mail.ru>
2019-01-04 19:49:32 +0700

    Модели

--- evl/controllers/ProjectC.cs
+++ evl/controllers/ProjectC.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using evl.models;
 
 namespace evl.controllers
 {
@@ -12,22 +11,12 @@ namespace evl.controllers
     /// </summary>
     class ProjectC
     {
-        List<Project> projects = new List<Project>();
-
         public void AddProject(string name, string date, string description)
         {
-            Project p = new Project(name, date, description);
-            projects.Add(p);
         }
 
         public void DeleteProject(string name, string date, string description)
         {
-            Project p = new Project(name, date, description);
-            try {
-                projects.Remove(p);
-            } catch (Exception ex) {
-
-            }
         }
 
         public void SaveProjects()
