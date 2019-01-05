7cece95054578d310d85d2dfd0bd69821b9931bd
Dmitry <dmitri.b.98.08@mail.ru>
2019-01-05 14:32:25 +0700

    main window

--- EVL/MainWindow.xaml.cs
+++ EVL/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EVL.Views;
 
 namespace EVL
 {
@@ -23,6 +24,22 @@ namespace EVL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ImportWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(new DataImport());
+        }
+
+        private void ProjectsWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(new Projects());
+        }
+
+        private void OpenWindow(Window win)
+        {
+            win.Show();
+            Close();
         }
     }
 }
