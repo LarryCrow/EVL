3f5b4e8fed8570ebfe90971a793c6b4aaadd8d42
Dmitry <dmitri.b.98.08@mail.ru>
2019-01-20 00:20:39 +0700

    model for binding with table

---
+++ EVL/Model/ClientRatingQuestionAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVL.Model
+{
+    public class ClientRatingQuestionAnswer
+    {
+        public int QuestionId { get; set; }
+        public string QuestionName { get; set; }
+        public string QuestionDescription { get; set; }
+        public string QuestionPurposeName { get; set; }
+
+        public double Answer { get; set; }
+    }
+}
---
+++ EVL/Model/MetricQuestionAnswers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVL.Model
+{
+    public class MetricQuestionAnswer
+    {
+        public int QuestionId { get; set; }
+        public string QuestionName { get; set; }
+        public string QuestionDescription { get; set; }
+        public string QuestionPurposeName { get; set; }
+        
+        public List<string> Answers { get; set; }
+        public string SelectedAnswer { get; set; }
+    }
+}
